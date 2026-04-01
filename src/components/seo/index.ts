c3b9055b5b8b5698764629d872a65d8358187fd1
dezzydez007 <nkefuan@yahoo.com>
2026-04-01 23:08:12 +0400

    refactor: move font imports from JS to CSS for better loading

--- src/components/seo/index.ts
+++ src/components/seo/index.ts
@@ -2,5 +2,3 @@ export { default as SEO } from './SEO';
 export { default as OrganizationSchema } from './OrganizationSchema';
 export { default as ContactPageSchema } from './ContactPageSchema';
 export { default as FAQSchema } from './FAQSchema';
-export { default as ContactPageSchema } from './ContactPageSchema';
-export { default as FAQSchema } from './FAQSchema';
